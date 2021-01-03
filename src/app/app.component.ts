cb5da839ce2995d0aa52768326256ad3577945d1
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2021-01-03 22:57:02 +0500

    responsive menu done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-app';
+  opened: boolean = true;
+  mode: string = 'side';
+
+  toggleSidenav() {
+    this.opened = !this.opened;
+  }
+
+  ngOnInit(): void {
+    let width = window.innerWidth;
+
+    if (width < 769) {
+      this.opened = false;
+      this.mode = 'over';
+    } else {
+      this.opened = true;
+      this.mode = 'side';
+    }
+  }
+
 }
