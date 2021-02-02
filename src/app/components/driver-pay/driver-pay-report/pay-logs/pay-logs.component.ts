016b2c49012ce2e1a3b9822fd1e747aafe9e2ba2
Awais Shaikh <awaisshaikh.official@gmail.com>
2021-02-02 21:06:10 +0500

    pay-report and pay-logs all done without checkbox button

--- src/app/components/driver-pay/driver-pay-report/pay-logs/pay-logs.component.ts
+++ src/app/components/driver-pay/driver-pay-report/pay-logs/pay-logs.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PayLogsComponent implements OnInit {
 
+  
+  ELEMENT_DATA: any = [
+    {pay_peroid: '19/9/2020 - 25/2/2021', driver_name: 'Ali Al-Abas', net_pay: 'total $', generated_on: 'date', by: 'admin name', show_it_on_driver_app: '', download: 'download', actions: ''},
+  
+  ];
+
+  displayedColumns: string[] = ['pay_peroid', 'driver_name', 'net_pay', 'generated_on', 'by', 'show_it_on_driver_app', 'download', 'actions'];
+  dataSource = this.ELEMENT_DATA;
+
   constructor() { }
 
   ngOnInit(): void {
